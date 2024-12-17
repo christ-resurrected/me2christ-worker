ac2dc1747f87b4ddaaadaf872b6989f2c156d8b9
christ-resurrected <dev@me2christ.com>
2024-12-17 10:20:53 +0000

    enable email

--- contact.js
+++ contact.js
@@ -1,5 +1,5 @@
-// import { EmailMessage } from 'cloudflare:email'
-// import { createMimeMessage } from 'mimetext'
+import { EmailMessage } from 'cloudflare:email'
+import { createMimeMessage } from 'mimetext'
 
 export default {
   async fetch(request, env) {
@@ -10,33 +10,32 @@ export default {
 
       const ip = request.headers.get("CF-Connecting-IP");
       const formData = await request.formData();
-      const name = formData.get("name");
-      const email = formData.get("email");
-      const message = formData.get("message");
+      const contact_name = formData.get("name");
+      const contact_email = formData.get("email");
+      const contact_message = formData.get("message");
       // const token = formData.get("cf-turnstile-response");
       // const tokenValidated = await validateToken(ip, token);
       // if (!tokenValidated) {
       //   return new Response("Token validation failed", { status: 403 });
       // }
 
-      console.log(`handleRequest: ${ip} ${name}, ${email}, ${message}`);
+      console.log(`handleRequest: ${ip} ${contact_name}, ${contact_email}, ${contact_message}`);
       console.log(`env: ${env.EMAIL_WORKER_ADDRESS} ${env.EMAIL_FORWARD_ADDRESS}`);
-      console.log(`vars: ${env.S} ${env.X}`);
 
-      // const msg = createMimeMessage()
-      // // msg.setSender(env.EMAIL_WORKER_ADDRESS)
-      // msg.setSender(email)
-      // msg.setRecipient(env.EMAIL_FORWARD_ADDRESS)
-      // msg.setSubject('Worker POST')
-      // msg.addMessage({
-      //   contentType: 'text/plain',
-      //   data: message,
-      // })
-      //
-      // console.log(msg)
-      //
-      // var em = new EmailMessage(env.EMAIL_WORKER_ADDRESS, env.EMAIL_FORWARD_ADDRESS, msg.asRaw())
-      // await env.CF_MAILER.send(em)
+      const msg = createMimeMessage()
+      // msg.setSender(env.EMAIL_WORKER_ADDRESS)
+      msg.setSender(contact_email)
+      msg.setRecipient(env.EMAIL_FORWARD_ADDRESS)
+      msg.setSubject('Worker POST')
+      msg.addMessage({
+        contentType: 'text/plain',
+        data: contact_message,
+      })
+
+      console.log(msg)
+
+      var em = new EmailMessage(env.EMAIL_WORKER_ADDRESS, env.EMAIL_FORWARD_ADDRESS, msg.asRaw())
+      await env.SEND_EMAIL.send(em)
       return new Response("OK", { status: 200 });
 
     } catch (e) {
