78d73b738ea7dc5b498a4a131fd63ea3d6b33b76
christ-resurrected <dev@me2christ.com>
2024-12-17 09:30:48 +0000

    cleardown

--- contact.js
+++ contact.js
@@ -1,44 +1,46 @@
-import { EmailMessage } from 'cloudflare:email'
-import { createMimeMessage } from 'mimetext'
+// import { EmailMessage } from 'cloudflare:email'
+// import { createMimeMessage } from 'mimetext'
 
-export async function onRequestPost({ request, env }) {
-  try {
-    if (request.method !== 'POST') {
-      return new Response('Method Not Allowed', { status: 405 })
+export default {
+  async fetch(request, env) {
+    try {
+      if (request.method !== 'POST') {
+        return new Response('Method Not Allowed', { status: 405 })
+      }
+
+      const ip = request.headers.get("CF-Connecting-IP");
+      const formData = await request.formData();
+      const name = formData.get("name");
+      const email = formData.get("email");
+      const message = formData.get("message");
+      // const token = formData.get("cf-turnstile-response");
+      // const tokenValidated = await validateToken(ip, token);
+      // if (!tokenValidated) {
+      //   return new Response("Token validation failed", { status: 403 });
+      // }
+
+      console.log(`handleRequest: ${ip} ${name}, ${email}, ${message}`);
+      console.log(`env: ${env.EMAIL_WORKER_ADDRESS} ${env.EMAIL_FORWARD_ADDRESS}`);
+
+      // const msg = createMimeMessage()
+      // // msg.setSender(env.EMAIL_WORKER_ADDRESS)
+      // msg.setSender(email)
+      // msg.setRecipient(env.EMAIL_FORWARD_ADDRESS)
+      // msg.setSubject('Worker POST')
+      // msg.addMessage({
+      //   contentType: 'text/plain',
+      //   data: message,
+      // })
+      //
+      // console.log(msg)
+      //
+      // var em = new EmailMessage(env.EMAIL_WORKER_ADDRESS, env.EMAIL_FORWARD_ADDRESS, msg.asRaw())
+      // await env.CF_MAILER.send(em)
+      return new Response("OK", { status: 200 });
+
+    } catch (e) {
+      console.error(e);
+      return new Response("Error sending message", { status: 500 });
     }
-
-    const ip = request.headers.get("CF-Connecting-IP");
-    const formData = await request.formData();
-    const name = formData.get("name");
-    const email = formData.get("email");
-    const message = formData.get("message");
-    // const token = formData.get("cf-turnstile-response");
-    // const tokenValidated = await validateToken(ip, token);
-    // if (!tokenValidated) {
-    //   return new Response("Token validation failed", { status: 403 });
-    // }
-
-    console.log(`handleRequest: ${ip} ${name}, ${email}, ${message}`);
-    console.log(`env: ${env.EMAIL_WORKER_ADDRESS} ${env.EMAIL_FORWARD_ADDRESS}`);
-
-    const msg = createMimeMessage()
-    // msg.setSender(env.EMAIL_WORKER_ADDRESS)
-    msg.setSender(email)
-    msg.setRecipient(env.EMAIL_FORWARD_ADDRESS)
-    msg.setSubject('Worker POST')
-    msg.addMessage({
-      contentType: 'text/plain',
-      data: message,
-    })
-
-    console.log(msg)
-
-    var em = new EmailMessage(env.EMAIL_WORKER_ADDRESS, env.EMAIL_FORWARD_ADDRESS, msg.asRaw())
-    await env.CF_MAILER.send(em)
-    return new Response("OK", { status: 200 });
-
-  } catch (e) {
-    console.error(e);
-    return new Response("Error sending message", { status: 500 });
   }
 }
