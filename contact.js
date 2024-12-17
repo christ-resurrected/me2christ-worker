c0fe99f2e84ed5dd2f6a6c4362453acae00df497
christ-resurrected <dev@me2christ.com>
2024-12-17 15:20:45 +0000

    add turnstile

--- contact.js
+++ contact.js
@@ -7,23 +7,27 @@ export default {
       if (request.method !== 'POST') {
         return new Response('Method Not Allowed', { status: 405 })
       }
-      const formData = await request.formData();
-      // await validateToken(formData);
-      await forwardMessage(formData, env);
-      return new Response("OK", { status: 200 });
+      const ip = request.headers.get("CF-Connecting-IP")
+      const formData = await request.formData()
+      const tokenValidated = await validateToken(formData, ip)
+      if (!tokenValidated) {
+        return new Response("Token validation failed", { status: 403 })
+      }
+      await forwardMessage(formData, env)
+      return new Response("OK", { status: 200 })
     } catch (e) {
       console.error(e);
-      return new Response("Error sending message", { status: 500 });
+      return new Response("Error sending message", { status: 500 })
     }
   }
 }
 
 async function forwardMessage(formData, env) {
-  const contact_name = formData.get("name");
-  const contact_email = formData.get("email");
-  const contact_message = formData.get("message");
-  console.log(`forwardMessage: ${contact_name}, ${contact_email}, ${contact_message}`);
-  // console.log(`env: ${env.EMAIL_WORKER_ADDRESS} ${env.EMAIL_FORWARD_ADDRESS}`);
+  const contact_name = formData.get("name")
+  const contact_email = formData.get("email")
+  const contact_message = formData.get("message")
+  console.log(`forwardMessage: ${contact_name}, ${contact_email}, ${contact_message}`)
+  // console.log(`env: ${env.EMAIL_WORKER_ADDRESS} ${env.EMAIL_FORWARD_ADDRESS}`)
 
   const msg = createMimeMessage()
   msg.setSender({ name: 'no-reply', addr: env.EMAIL_WORKER_ADDRESS })
@@ -39,9 +43,22 @@ async function forwardMessage(formData, env) {
   console.log('...done')
 }
 
-// async function validateToken(formData, env) {
-//   const token = formData.get("cf-turnstile-response");
-//   if (!tokenValidated) {
-//     return new Response("Token validation failed", { status: 403 });
-//   }
-// }
+async function validateToken(formData, ip) {
+  const TURNSTILE_SECRET_KEY = '1x0000000000000000000000000000000AA'
+  const token = formData.get("cf-turnstile-response")
+  console.log(`token: ${token}`)
+
+  const body = new FormData()
+  body.append('secret', TURNSTILE_SECRET_KEY)
+  body.append('response', token)
+  body.append('remoteip', ip)
+
+  const url = 'https://challenges.cloudflare.com/turnstile/v0/siteverify'
+  const result = await fetch(url, {
+    body: body,
+    method: 'POST',
+  });
+
+  const outcome = await result.json()
+  return outcome.success;
+}
