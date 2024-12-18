eb19e79be9e58103bd1c71c98a636b6ee9ec9995
christ-resurrected <dev@me2christ.com>
2024-12-18 04:31:35 +0000

    add DISABLE_WORKER flag

--- contact.js
+++ contact.js
@@ -11,7 +11,9 @@ export default {
         return r
       }
 
-      if (request.method !== 'POST') return generateResponse('Method Not Allowed', 405)
+      if (request.method !== 'POST') return generateResponse(`Method ${request.method} not allowed`, 405)
+      if (env.DISABLE_WORKER) return generateResponse('Service unavailable', 503)
+
       const ip = request.headers.get("CF-Connecting-IP")
       const formData = await request.formData()
       const tokenValidated = await validateToken(formData, env, ip)
@@ -30,7 +32,7 @@ async function forwardMessage(formData, env) {
   const contact_email = formData.get('email')
   const contact_message = formData.get('message')
   console.log(`forwardMessage: ${contact_name}, ${contact_email}, ${contact_message}`)
-  // console.log(`env: ${env.EMAIL_WORKER_ADDRESS} ${env.EMAIL_FORWARD_ADDRESS}`)
+  // console.log(`env: ${ env.EMAIL_WORKER_ADDRESS } ${ env.EMAIL_FORWARD_ADDRESS }`)
 
   const msg = createMimeMessage()
   msg.setSender({ name: 'no-reply', addr: env.EMAIL_WORKER_ADDRESS })
